da8eb973b4659d2ab4eeaaaeee7961e70a1b2a2e
Prince <princegujjar1.pp@gmail.com>
2022-10-05 01:59:45 +0530

    fragments are added(Not working properly)

--- app/src/main/java/com/example/testapp1/MainActivity.kt
+++ app/src/main/java/com/example/testapp1/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.testapp1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.testapp1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val Home =HomeFragment()
+        val Article =Articlefragment()
+        val Settings = SettingFragment()
+        setCurrentFragment(Home)
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+
+            when (it.itemId) {
+                R.id.home -> setCurrentFragment(Home)
+                R.id.article -> setCurrentFragment(Article)
+                R.id.setting -> setCurrentFragment(Settings)
+            }
+        true
+
+        }
+
     }
+private fun setCurrentFragment(fragment: Fragment){
+    supportFragmentManager.beginTransaction().apply {
+        replace(R.id.frame_layout,fragment)
+        commit()
+    }
+}
+
 }
